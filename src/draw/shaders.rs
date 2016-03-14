c5cc6fcc2ff21ae7a6d2fc557dbe73942179d0da
Thomas Colliers <mail@thomascolliers.com>
2016-03-14 20:12:06 +0700

    Working on the shader manager

--- src/draw/shaders.rs
+++ src/draw/shaders.rs
@@ -8,6 +8,13 @@ pub struct ShaderManager {
 	shaders:HashMap<String,Program>
 }
 
+/*
+ The shader manager should load and cache files and compile the shaders.
+ By doing this, display objects will be able to retrieve their shaders from
+ the manager. Every scene object should have a shader assigned to it as well
+ as other basic properties such as position
+*/
+
 impl ShaderManager {
 	pub fn new<F>(display: &F) -> ShaderManager where F: Facade + Clone {
 		ShaderManager {
@@ -15,3 +22,32 @@ impl ShaderManager {
 		}
 	}
 }
+
+struct ShaderSource {
+	vertex:String,
+	fragment:String,
+	geometry:Option<String>,
+}
+
+
+/* DEFINE SOME SHADER PROGRAMS */
+let identity_vertex:String = String::from(r#"
+	#version 430
+
+	in vec2 position;
+
+	void main(){
+		gl_Position = vec4(position, 0.0, 1.0);
+	}
+"#);
+
+const identity_fragment:String = String::from(r#"
+	#version 430
+
+	out vec4 color;
+
+	void main(){
+		color = vec4(1.0, 0.0, 0.0, 1.0);
+	}
+"#);
+
